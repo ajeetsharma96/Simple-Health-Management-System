86fbc45339d5f3264619cff8c3593ee6ddedd144
ajeetsharma96 <ajeetsharma500045@gmail.com>
2026-01-04 19:29:01 +0530

    Written function to store and retrieve data

--- simpleHealthManagementSystem.py
+++ simpleHealthManagementSystem.py
@@ -1,3 +1,25 @@
+def retr_data():
+    import os
+    print(f"{"-"*10}Profiles{"-"*10}")
+    print("1.Ajeet\n2.Rahul\n3.Akash\n4.Rudra\n5.Rakesh")
+    user=input("Choose Name from above names: ")
+    if user in files:
+        print(f"{"-"*10}Categories{"-"*10}")
+        print("1.Food\n2.Exercise")
+        categories=int(input("Enter the categories: "))
+
+        file_name=files[user][categories]
+
+        if os.path.exists(file_name):
+            with open(file_name, "r") as r:
+                print(f"{"-"*10}Stored Data{"-"*10}")
+                print(r.read())
+        else:
+            print(f"You have nothing in '{file_name}' to show")
+    else:
+        print("Profile not found! Please Enter an existing Profile Name")
+
+
 def main():
     print(f"{"-"*20}HEALTH MANAGEMENT SYSTEM{"-"*20}")
 
